25e09892ad55825a6591a8a04ec4a60d8de3edc7
raunerlucas <raunerlucas@gmail.com>
2025-08-10 15:49:03 -0300

    Adiciona suporte a estratégia de desconto na classe Venda e inclui dependência do Lombok

--- exercicios/calculo-desconto/src/main/java/com/manoelcampos/desconto/Venda.java
+++ exercicios/calculo-desconto/src/main/java/com/manoelcampos/desconto/Venda.java
@@ -1,16 +1,20 @@
 package com.manoelcampos.desconto;
 
+import lombok.Data;
+
 import java.time.LocalDate;
 
 /**
  * Venda realizada para um {@link Cliente}.
  * @author Manoel Campos da Silva Filho
  */
+@Data
 public class Venda {
     private long id;
     private Cliente cliente;
     private LocalDate data;
     private double valorTotal;
+    private CalculadoraDesconto estrategiaDesconto;
 
     public Venda() {/**/}
 
@@ -18,5 +22,18 @@ public class Venda {
         this.cliente = cliente;
         this.data = data;
         this.valorTotal = valorTotal;
+    }
+
+    /**
+     * Calcula o valor do desconto aplicável a esta venda, de acordo com a
+     * estratégia de desconto definida.
+     * @return Valor do desconto. Se nenhuma estratégia de desconto foi definida,
+     * retorna 0.0 (zero).
+     */
+    public double calcularDesconto() {
+        if (estrategiaDesconto == null) {
+            return 0.0;
+        }
+        return estrategiaDesconto.calcularDesconto(this);
     }
 }
